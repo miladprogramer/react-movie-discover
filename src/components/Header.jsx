4d57bdb323b35cf0d9911e07274396721534030d
meysam <m.monfared83@gmail.com>
2023-08-16 23:25:51 +0330

    create pages

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+<>
+    <span className='header'>Header-Movie-search</span>
+
+
+    </>
+  )
+}
+
+export default Header
---
+++ src/pages/Series.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Series = () => {
+  return (
+    <div>series</div>
+  )
+}
+
+export default Series
