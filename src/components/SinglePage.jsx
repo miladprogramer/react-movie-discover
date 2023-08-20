7e6dc0b6c733b0bb8a43b0b00f8952bee2657166
meysam <m.monfared83@gmail.com>
2023-08-20 18:08:00 +0330

    fetching trending

---
+++ src/components/SinglePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SinglePage = ({
+    id, title,poster,media_type,release_date,vote_averag
+}) => {
+  return (
+    <div>{title}</div>
+  )
+}
+
+export default SinglePage
--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -1,28 +1,84 @@
 import React from 'react'
 import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import SinglePage from '../components/SinglePage';
 
-const options = {
-  method: 'GET',
-  url: 'https://api.themoviedb.org/3/trending/all/day',
-  params: {language: 'en-US'},
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWE4NDM3NjIzNTRlYmRiM2QyNDlhMTUxNGIwY2M5ZSIsInN1YiI6IjY0ZTEzMDFiMzcxMDk3MDBhYzQ1NTlmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0kfWu571mqNTnlMR_WRctkNP-_M5eysrL3K7uB_6njI'
-  }
-};
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
 
 const Trending = () => {
+
+  const [content,setContent]=useState([]);
+
+  useEffect(() =>{
+    // const options = {
+    //   method: 'GET',
+    //   url: 'https://api.themoviedb.org/3/trending/all/day',
+    //   params: {language: 'en-US'},
+    //   headers: {
+    //     accept: 'application/json',
+    //     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWE4NDM3NjIzNTRlYmRiM2QyNDlhMTUxNGIwY2M5ZSIsInN1YiI6IjY0ZTEzMDFiMzcxMDk3MDBhYzQ1NTlmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0kfWu571mqNTnlMR_WRctkNP-_M5eysrL3K7uB_6njI'
+    //   }
+    // };
+    
+    // axios
+    //   .request(options)
+    //   .then(function (response) {
+    //     console.error(response.data);
+    //     setContent(response.data)
+    //   })
+    //   .catch(function (error) {
+    //     console.error(error);
+    //   });
+
+    // const fetchTrending=async()=>{
+    //   const { data }= await axios.get(
+    //     "https://api.themoviedb.org/3/trending/all/day?access_token=eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWE4NDM3NjIzNTRlYmRiM2QyNDlhMTUxNGIwY2M5ZSIsInN1YiI6IjY0ZTEzMDFiMzcxMDk3MDBhYzQ1NTlmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0kfWu571mqNTnlMR_WRctkNP-_M5eysrL3K7uB_6njI"
+    //   )
+    //   console.log(data)
+    // }
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWE4NDM3NjIzNTRlYmRiM2QyNDlhMTUxNGIwY2M5ZSIsInN1YiI6IjY0ZTEzMDFiMzcxMDk3MDBhYzQ1NTlmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0kfWu571mqNTnlMR_WRctkNP-_M5eysrL3K7uB_6njI'
+      }
+    };
+    
+    fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
+      .then(response => response.json())
+      .then(response => {
+     
+        setContent(response.results)
+      })
+      .catch(err => console.error(err));
+  },[])
+
+ 
+
   return (
+    <>
     <div>Trending</div>
+   <div>
+   {
+
+     content && content.map( (c) =>(
+      <SinglePage 
+      key={c.id}
+      id={c.id} 
+      title={c.title}
+       poster={c.poster_pat}
+        media_type={c.media_type}
+        release_date={c.release_date}
+        vote_averag={c.vote_averag} />
+     )) 
+     
+     }
+   </div>
+
+
+    </>
   )
 }
 
